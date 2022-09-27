8f6ce4200e026881c73b17543ddd1ccc67d1247a
LukashevichEvgeniy <humansyndr8me@gmail.com>
2022-09-27 17:47:48 +0800

    the 1st lecture added

---
+++ lectures/l1_HalloPython/hallo.py
@@ -0,0 +1,90 @@
+# a = 'Хэллоу Ворлд!'
+# print(a)
+
+# a = 123
+# b = 2.155
+# c = None
+# e = 'hi, bro'
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(e))
+
+
+# stroka1 = "это 'прекраснейшая' пора"
+# stroka2 = 'это "великолепная" пора'
+# print(stroka1)
+# print(stroka2)
+
+
+# s = 'it\'s my life '
+# s2 = 'ну вот мы и \nспустились'
+# print(s)
+# print(s2)
+
+
+# a = 123
+# b = 12.5
+# c = 'hallo'
+
+# print(a, b, c)
+# print(a, 'плюс', b, 'сказали', c)
+# print(f'{a} и {b} сказали нам {c}')
+
+
+# Логическая переменная
+
+# f = True
+# print(f)
+
+# list = [1, 44, 3.2]
+# print(list)
+
+
+# Ввод от пользователя
+
+# print("Пожалуйста, задайте переменную a: ")
+# a = float(input())
+# print("Пожалуйста, задайте переменную b: ")
+# b = float(input())
+
+# sum = a + b
+
+# print(f'{a} + {b} = {sum}')
+
+
+# Математические операции
+# Унарные плюс и минус
+
+# a = +23
+# b = -17
+# sum = a - b
+# print(sum)
+
+
+# Логические операции
+
+# a= 1 < 3 < 5 < 10
+# print(a)
+
+
+# f = [1, 2, 5, 7]
+
+# odd1 = f[0] % 2 == 0
+# odd2 = not f[0] % 2
+
+# print(odd1)
+# print(odd2)
+
+
+# IF ELSE
+
+a = int(input('a = '))
+b = int(input('b = '))
+
+if a > b:
+    print(f'The MAX is {a}')
+else:
+    print(f'The MAX is {b}')
+
